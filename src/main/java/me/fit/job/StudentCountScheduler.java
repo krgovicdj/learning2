a59a11b1bbac97d478fbe34e55c996d933b8917a
Djordje Krgovic <djordjekrgovic.sa@gmail.com>
2026-06-02 17:31:23 +0200

    scheduler

---
+++ src/main/java/me/fit/job/StudentCountScheduler.java
@@ -0,0 +1,19 @@
+package me.fit.job;
+
+import io.quarkus.scheduler.Scheduled;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+
+@ApplicationScoped
+public class StudentCountScheduler {
+
+    @Inject
+    EntityManager em;
+
+    @Scheduled(every = "15s")
+    void logStudentCount() {
+        Long studentCount = em.createQuery("SELECT COUNT(s) FROM Student s", Long.class).getSingleResult();
+        System.out.println("[SCHEDULER] Students in dattabase: " + studentCount + " | time: " + java.time.LocalDateTime.now());
+    }
+}
